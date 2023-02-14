5d69dfabce83167855e4cec6f27a1074b0395ef9
Felipe Toscano da Silva <felipetoscano02@gmail.com>
2023-02-13 21:26:16 -0300

    feat: melhorando validação de cadastro e atualização

--- src/main/java/br/com/eletricacontinentalmotores/api/models/CustomerModel.java
+++ src/main/java/br/com/eletricacontinentalmotores/api/models/CustomerModel.java
@@ -1,7 +1,7 @@
 package br.com.eletricacontinentalmotores.api.models;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -14,17 +14,17 @@ public class CustomerModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @NotNull
+    @NotEmpty(message = "O primeiro nome não pode ser vazio")
     @Size(max = 40, message = "O primeiro nome deve ter menos de 40 caracteres.")
     @Column(name = "first_name")
     private String firstName;
 
-    @NotNull
+    @NotEmpty(message = "O último nome não pode ser vazio")
     @Size(max = 80, message = "O último nome deve ter menos de 80 caracteres.")
     @Column(name = "last_name")
     private String lastName;
 
-    @NotNull
+    @NotEmpty(message = "O celular não pode ser vazio")
     @Pattern(regexp="^\\d{11}",message="O celular deve ter 11 números")
     @Column(name = "cellphone")
     private String cellphone;
